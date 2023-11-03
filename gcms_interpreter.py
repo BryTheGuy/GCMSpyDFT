94e98e38e35108fa06d8f16cd046e460fda2d828
BryTheGuy <44144252+BryTheGuy@users.noreply.github.com>
2023-11-03 15:28:56 -0700

    Added gcms_interpreter.py

---
+++ gcms_interpreter.py
@@ -0,0 +1,64 @@
+def read_to_list(input_file: str) -> list:
+    """  Reads a file to a list and returns the list.  """
+    lines: list
+    with open(input_file) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def content_finder(lines: list, sep: str = "___") -> int:
+    """  Scans list for a separator line and returns the index or -1 of not found.  """
+    for line in lines:
+        if sep in line:
+            return lines.index(line)
+    return -1
+
+
+def trim_header(lines: list, sep_index: int) -> list:
+    """  Removes sep_index lines from head of list and returns a new list.  """
+    if sep_index < 0:
+        raise IndexError("Make sure file contains header.")
+    return lines[sep_index + 1:]
+
+
+def is_peak_line(line: str) -> bool:    # TODO: Make more robust
+    """  Checks if second char is a digit and returns the bool.  """
+    if len(line) > 2:
+        return line[2].isdigit()
+    else:
+        return False
+
+
+def peak_blocker(lines: list) -> list[str]:
+    """
+    Collects all lines in a peak into a list.
+
+    :param lines: Lines to read from to create a peak block.
+    :return: A list of lines in one peak.
+    """
+    while lines:
+        current_line = lines.pop(0)
+        if is_peak_line(current_line):
+            peak = []
+            while True:
+                peak.append(current_line)
+                current_line = lines.pop(0)
+                if not is_peak_line(current_line):
+                    continue
+                else:
+                    break
+            return peak
+    return []
+
+
+def peak_agg(lines: list) -> list[list]:  # TODO: Might switch to a recursive
+    """
+    Collects peak blocks into a list.
+
+    :param lines: Lines to read from to create list of blocks.
+    :return: List of peak block lists.
+    """
+    blocks = []
+    while len(lines):
+        blocks.append(peak_blocker(lines))
+    return blocks
