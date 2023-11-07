e2042de52a4f9136bc96b08c132cf602adeb5162
BryTheGuy <44144252+BryTheGuy@users.noreply.github.com>
2023-11-06 23:04:29 -0800

    write and main added

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os.path
 
 
 class Config:
-    config_path = "./config.ini"
+
+    def __init__(self):
+        self.config_path = "./config.ini"
 
     def make_config(self):
         if os.path.isfile(self.config_path):
@@ -12,21 +14,29 @@ class Config:
         elif not os.path.isfile(self.config_path):
             config = configparser.ConfigParser()
             config['Environment'] = {
-                "InputFile": './input.txt',
-                "OutputDir": './output/',
+                "Input File": './input.txt',
+                "Output Dir": './output/',
             }
             config['File'] = {
                 "Cores": '28',
                 "Memory": '50GB',
                 "Checkpoint": 'Yes',
-                "OldCheckpoint": 'No',
+                "Old Checkpoint": 'No',
             }
             config['Route'] = {
                 "Theory": 'B3LYP',
-                "Basis_Set": '6-31G',
-                "Calc_Type": 'opt'
+                "Basis Set": '6-31G',
+                "Calc Type": 'opt'
             }
-            config['MoleculeSpec'] = {
+            config['Molecule Specs'] = {
                 "Charge": '0',
                 "Spin": '1'
             }
+
+            with open(self.config_path, 'w') as config_file:
+                config.write(config_file)
+
+
+if __name__ == '__main__':
+    f = Config()
+    f.make_config()
