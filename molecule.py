09480c47e495ab22acc6412241ef4b6c2266d3b2
BryTheGuy <44144252+BryTheGuy@users.noreply.github.com>
2023-11-13 15:53:12 -0800

    moved name opsin to molecule.py

--- molecule.py
+++ molecule.py
@@ -1,5 +1,9 @@
-from openbabel import openbabel
-from cas_converter import name_to_format
+from openbabel import openbabel, pybel
+from py2opsin import py2opsin as opsin
+
+import config
+
+config_file = config.Config()
 
 
 class Molecule:
@@ -7,14 +11,35 @@ class Molecule:
     name_no_space: str
     smiles: str
     cml: str
-    mol: openbabel.OBMol
+    mol: pybel.Molecule
     charge: int
     spin: int
 
-    def generate_smi(self):
-        if self.smiles:
-            self.smiles = name_to_format(self.name, 'SMILES')
+    def name_strip(self):
+        self.name_no_space = self.name.strip().replace(' ', '_')
 
-    def generate_cml(self):
-        if self.cml:
-            self.cml = name_to_format(self.name, 'CML')
+    def name_to_smi(self):
+        if not self.smiles:
+            self.smiles = self.name_to_format('SMILES')
+
+    def name_to_cml(self):
+        if not self.cml:
+            self.cml = self.name_to_format('CML')
+
+    def name_to_format(self, out_format: str = config_file.opsin_format()) -> str | list:
+        """
+        Input simplifier for py2opsin.
+
+        Use list of names for batch processing as
+        there is a performance boost.
+        :param out_format:
+        :return:
+        """
+        return opsin(
+            chemical_name=self.name,
+            output_format=out_format,
+            allow_acid=config_file.acid(),
+            allow_radicals=config_file.radicals(),
+            allow_bad_stereo=config_file.bad_stereo(),
+            wildcard_radicals=config_file.wildcard_radicals())
+
