e5265f68f38074a709bef674ef5cf769e3c38d3c
BryTheGuy <44144252+BryTheGuy@users.noreply.github.com>
2023-11-21 11:17:35 -0800

    moved to functions

--- main.py
+++ main.py
@@ -1,100 +1,147 @@
 import argparse
+import logging
+import os
 import pathlib
 import sys
 
-from config import cgf
 
-parser = argparse.ArgumentParser(
-    prog="DFT_GC-MS",
-    description="Program to convert an output of a GC-MS run into \n"
-                "computational chemistry input files.")
+def command_line():
+    parser = argparse.ArgumentParser(
+        prog="DFT_GC-MS",
+        description="Program to convert an output of a GC-MS run into computational chemistry input files.")
 
-parser.add_argument('infile',
-                    nargs='?',
-                    type=argparse.FileType('r'),
-                    default=sys.stdin,
-                    help="Input file name.")
+    group = parser.add_mutually_exclusive_group()
 
-parser.add_argument('-v',
-                    '--version',
-                    action='version',
-                    version='%(prog)s v0.1')
+    group.add_argument("-v",
+                       "--verbosity",
+                       action="store_true",
+                       help="make verbose")
 
-parser.add_argument('-V',
-                    '--Verbose',
-                    action='store_true')
+    group.add_argument("-q",
+                       "--quiet",
+                       action="store_true",
+                       help="silence output")
 
-parser.add_argument('-o',
-                    '--output',
-                    type=pathlib.Path,
-                    help="Output directory.")
+    parser.add_argument('infile',
+                        nargs='?',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help="Input file name.")
 
-parser.add_argument('-c',
-                    '--cores',
-                    type=int,
-                    help="Number of cpu cores.")
+    parser.add_argument('-v',
+                        '--version',
+                        action='version',
+                        version='%(prog)s v0.1')
 
-parser.add_argument('-m',
-                    '--memory',
-                    type=int,
-                    help="Maximum allowed memory.")
+    parser.add_argument('-o',
+                        '--output',
+                        type=pathlib.Path,
+                        help="Output directory.")
 
-parser.add_argument('-C',
-                    '--checkpoint',
-                    action='store_true',
-                    help="Create checkpoint.")
+    parser.add_argument('-c',
+                        '--cores',
+                        type=int,
+                        help="Number of cpu cores.")
 
-parser.add_argument('-t',
-                    '--theory',
-                    type=str,
-                    help="Functional method to use for all input files.")
+    parser.add_argument('-m',
+                        '--memory',
+                        type=int,
+                        help="Maximum allowed memory.")
 
-parser.add_argument('-b',
-                    '--basis',
-                    type=str,
-                    help="Basis set to use for all input files.")
+    parser.add_argument('-C',
+                        '--checkpoint',
+                        action='store_true',
+                        help="Create checkpoint.")
 
-parser.add_argument('-T',
-                    '--type',
-                    choices=["SP", "Opt", "Freq"],
-                    nargs='*',
-                    help="Calculation type to preform. eg. 'SP' = 'Single point'")
+    parser.add_argument('-t',
+                        '--theory',
+                        type=str,
+                        help="Functional method to use for all input files.")
 
-parser.add_argument('--charge',
-                    type=int,
-                    help="Total Charge to assign each molecules. Overrides predicted charge.")
+    parser.add_argument('-b',
+                        '--basis',
+                        type=str,
+                        help="Basis set to use for all input files.")
 
-parser.add_argument('--spin',
-                    type=int,
-                    help="Multiplicity spin to assign each molecule. Overrides predicted spin.")
+    parser.add_argument('-T',
+                        '--type',
+                        choices=["SP", "Opt", "Freq"],
+                        nargs='*',
+                        help="Calculation type to preform. eg. 'SP' = 'Single point'")
 
-parser.add_argument('-M',
-                    '--modred',
-                    type=str,
-                    nargs='*',
-                    help="Modify redundant internal coordinate definitions to include at the end of each input file")
+    parser.add_argument('--charge',
+                        type=int,
+                        help="Total Charge to assign each molecules. Overrides predicted charge.")
 
-args = parser.parse_args()
+    parser.add_argument('--spin',
+                        type=int,
+                        help="Multiplicity spin to assign each molecule. Overrides predicted spin.")
 
-verbose_print = print if args.Verbose else lambda *a, **k: None
+    parser.add_argument('-M',
+                        '--modred',
+                        type=str,
+                        nargs='*',
+                        help="Modify redundant internal coordinate definitions to be include at "
+                             "the end of each input file.")
 
-if args.output:
-    cgf.output = args.output
-elif args.cores:
-    cgf.cores = args.cores
-elif args.memory:
-    cgf.memory = args.memory
-elif args.checkpoint:
-    cgf.memory = args.checkpoint
-elif args.theory:
-    cgf.theory = args.theory
-elif args.basis:
-    cgf.basis = args.basis
-elif args.type:
-    cgf.calc_type = args.type
-elif args.charge:
-    cgf.charge = args.charge
-elif args.spin:
-    cgf.spin = args.spin
-elif args.modred:
-    cgf.modred = args.modred
+    return parser.parse_args()
+
+
+def log_settings(arguments):
+    logger_setting = logging.getLogger('pyDFT')
+    # Logging setup
+    logger_setup = logging.getLogger('pyDFT')
+    logger_setup.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('pyDFT.log', mode='w')
+    fh.setLevel(level=logging.DEBUG)
+    # create formatter and add it to the handlers
+    fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(fh_formatter)
+    # add the handlers to the logger
+    logger_setup.addHandler(fh)
+
+    # create console handler
+    ch = logging.StreamHandler()
+    # create formatter and add it to the handlers
+    ch_formatter = logging.Formatter('%(levelname)s: %(message)s')
+    ch.setFormatter(ch_formatter)
+    if arguments.verbosity:
+        # create console handler with a higher log level
+        ch.setLevel(logging.INFO)
+        logger_setting.addHandler(ch)
+        logger_setting.debug("Cavitation")
+    elif arguments.quiet:
+        # create console handler with no logging
+        ch.setLevel(logging.CRITICAL + 1)
+        logger_setting.addHandler(ch)
+        logger_setting.debug("Quite running")
+    else:
+        # create console handler with a default log level
+        ch.setLevel(logging.WARNING)
+        logger_setting.addHandler(ch)
+        logger_setting.debug("Plain bagel")
+
+
+def configuration(arguments):
+    from config import cgf
+    configuration_logger = logging.getLogger('pyDFT')
+    # checks if configuration file exist
+    if os.path.isfile(cgf.config_path):
+        configuration_logger.info("Config file found, loading settings...")
+        cgf.read_config()
+    # if configuration file does not exist
+    else:
+        configuration_logger.info("Config file not found, writing settings...")
+        cgf.make_config(cgf.config_path)
+        cgf.read_config()
+
+
+if __name__ == '__main__':
+    # run things
+    args = command_line()
+    log_settings(args)
+    logger = logging.getLogger('pyDFT')
+    logger.debug("Starting configuration")
+    configuration(args)
